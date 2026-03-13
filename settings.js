50a973f7b2cde2e169758734e64d0508d07f4a3e
centralhex0 <centralhex0@gmail.com>
2026-03-13 08:24:02 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,24 @@
+global.API_KEYS = {
+  
+  // News
+  NEWS_API: "TA_CLE_NEWSAPI",
+
+  // Remove Background
+  REMOVE_BG: "TA_CLE_REMOVE_BG",
+
+  // Crypto
+  COINGECKO: "TA_CLE_COINGECKO",
+
+  // Stock market
+  ALPHA_VANTAGE: "TA_CLE_ALPHA_VANTAGE",
+
+  // Football
+  FOOTBALL_API: "TA_CLE_FOOTBALL",
+
+  // Cricket
+  CRICKET_API: "TA_CLE_CRICKET",
+
+  // AI
+  OPENAI: "TA_CLE_OPENAI"
+
+}
